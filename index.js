5e75d3a3fb9a0194676736b206cb99e2ff033f1d
IngridGdesigns <garciai@protonmail.com>
2024-04-04 16:11:25 -0700

    updated styles, added button id

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ let modal = document.getElementById("modal");
 btn.addEventListener("click", function(e){
     e.preventDefault();
     modal.style.display = "flex";
+    document.getElementById("py-form").reset();
 })
 
 btnRefresh.addEventListener("click", function(){
     modal.style.display = "none";
-    document.getElementById("py-form").reset();
+    
 })
